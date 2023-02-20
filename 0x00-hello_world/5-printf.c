4f8046aa25c9670e8ceefc9fb017268f0cbe50d9
Abdihafidh <abdihafidhhussein09@gmail.com>
2023-02-20 01:51:45 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * prints a line using printf.
  * Return (0)
- */
+*/
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,");
+ printf("with proper grammar, but the outcome is a piece of art,");
 
-  return (0);
+ return (0);
 }
