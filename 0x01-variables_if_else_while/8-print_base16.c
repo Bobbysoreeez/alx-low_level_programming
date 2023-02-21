182d9fda5bc401eed00b8655d1b9988683efaa3c
Abdihafidh <abdihafidhhussein09@gmail.com>
2023-02-21 05:22:32 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -this program prints the base 16 numbers
+ * Return:Always 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 16; a++)
+		putchar(a);
+
+	putchar('\n');
+
+	return (0);
+}
