f5a58ba681e6b33238f3f3817c8fce9cded8664c
Abdihafidh <abdihafidhhussein09@gmail.com>
2023-02-21 04:24:00 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints the numbers base ten incluuding zero
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		printf("%d", i);
+
+	printf("\n");
+
+	return (0);
+}
