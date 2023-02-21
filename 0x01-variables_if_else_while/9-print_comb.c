fbc10326f81e8d2c021e1e09cdc4f4dec624a3a4
Abdihafidh <abdihafidhhussein09@gmail.com>
2023-02-21 06:50:33 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 /**
  * main -prints all possible cominations of single digit numbers
+ * Return:Always 0
  */
 
 int main(void)
 {
-	int k, l;
+	int k;
 
 	for (k = 0; k < 10; k++)
+		putchar(k + '0');
 	{
-		for (l = 0; l < 10; l++)
-			putchar(k + '0');
-		putchar(l + '0');
-
-		if (k < 9 || l < 9)
+		if (k < 10)
 		{
-		putchar(',');
-	putchar(' ');
+			putchar(',');
+			putchar(' ');
 		}
 	}
-return (0);
+		return (0);
 }
