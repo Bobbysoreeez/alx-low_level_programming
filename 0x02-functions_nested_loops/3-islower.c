afb5e169641b55a0c9ebf32ca72de27a8e21632c
Abdihafidh <abdihafidhhussein09@gmail.com>
2023-02-22 00:42:36 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - program that checks for lowercase letters
+ * Return: 1 success
+ */
+int main(void)
+{
+	char chr = c;
+
+	if (islower(c))
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
