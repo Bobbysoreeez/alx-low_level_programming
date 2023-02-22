9ef07478959645a0d20ca157a4e2e23efc4be556
Abdihafidh <abdihafidhhussein09@gmail.com>
2023-02-22 07:08:20 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _abs -function that computes the absolute value of interger
+ * @n: the int to check
+ * Return: 0 success
+ */
+int _abs(int);
+{
+	int n;
+
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+
+	return (0);
+}
